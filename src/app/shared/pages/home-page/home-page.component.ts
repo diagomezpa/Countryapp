288713eee5c7b1d231d57a5265d0cf8b089745cc
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-04-29 10:23:43 -0500

    calls 9 video 118 Resolución de la tarea

--- src/app/shared/pages/home-page/home-page.component.ts
+++ src/app/shared/pages/home-page/home-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'share-home-page',
+  selector: 'shared-home-page',
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
