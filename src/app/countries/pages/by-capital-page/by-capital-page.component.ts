03a57ff449a707c8a3edcc09286cb2c96e40f153
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-04-29 15:38:46 -0500

    clase 9 video 122. Funcionamiento del SearchBox

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-by-capital-page',
   templateUrl: './by-capital-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class ByCapitalPageComponent {
-
+  searchByCapital(term: string): void {
+    console.log('desdecapital page');
+    console.log({ term });
+  }
 }
