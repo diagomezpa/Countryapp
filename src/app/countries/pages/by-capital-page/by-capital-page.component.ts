e992aa94b13628a579e648192141bf743ca5305c
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-04-29 22:07:47 -0500

    class 9 video 124. Mostrar los países en pantalla

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/country';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -6,8 +8,12 @@ import { Component } from '@angular/core';
   styles: ``,
 })
 export class ByCapitalPageComponent {
+  constructor(private countriesService: CountriesService) {}
+
+  public countries: Country[] = [];
   searchByCapital(term: string): void {
-    console.log('desdecapital page');
-    console.log({ term });
+    this.countriesService.searchCapital(term).subscribe((countries) => {
+      this.countries = countries;
+    });
   }
 }
