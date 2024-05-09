309686d55ba2aabae3bcb3a096b9b20f7ee866ea
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-05-08 20:50:32 -0500

    clase 10 video adingSpinner

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -11,9 +11,12 @@ export class ByCapitalPageComponent {
   constructor(private countriesService: CountriesService) {}
 
   public countries: Country[] = [];
+  public isLoading = false;
   searchByCapital(term: string): void {
+    this.isLoading = true;
     this.countriesService.searchCapital(term).subscribe((countries) => {
       this.countries = countries;
+      this.isLoading = false;
     });
   }
 }
