43722033a8ed6d8d1b02b63554b4724cd581913b
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-05-08 22:20:55 -0500

    clase 10 video 141. Buscar por region - Optimización

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -11,9 +13,18 @@ export class ByRegionPageComponent {
   constructor(private countriesService: CountriesService) {}
 
   public countries: Country[] = [];
-  searchByRegion(term: string): void {
-    console.log('entra en region');
-    this.countriesService.searchRegion(term).subscribe((countries) => {
+  public regions: Region[] = [
+    'Africa',
+    'Americas',
+    'Asia',
+    'Europe',
+    'Oceania',
+  ];
+  public selectedRegion?: Region;
+
+  searchByRegion(region: Region): void {
+    this.selectedRegion = region;
+    this.countriesService.searchRegion(region).subscribe((countries) => {
       this.countries = countries;
     });
   }
